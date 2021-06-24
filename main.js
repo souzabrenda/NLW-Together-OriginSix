440c2bf9cb27d70f58cea218242fcac108152bda
souzabrenda <souzabrenda.cello@gmail.com>
2021-06-24 09:41:27 -0300

    Scroll Reveal

--- main.js
+++ main.js
@@ -38,3 +38,23 @@ const swiper = new Swiper('.swiper-container', {
   mousewheel: true,
   keyboard: true
 })
+
+
+
+
+
+const scrollReveal =  ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 750,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+   #about .image, #about .text,
+   #services header, #services .card,
+   #testimonials header, #testimonials .testimonial,
+   #contact .text, #contact .links
+  
+  `,{ interval: 100})
